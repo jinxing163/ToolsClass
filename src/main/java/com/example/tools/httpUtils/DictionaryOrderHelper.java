a0bb66f313c980d48fbe906b39d4c81dec981252
jinxing <jinxing@able-elec.com>
2018-07-18 10:58:10 +0800

    http工具类 +字典排序

---
+++ src/main/java/com/example/tools/httpUtils/DictionaryOrderHelper.java
@@ -0,0 +1,44 @@
+package com.example.tools.httpUtils;
+
+
+import com.alibaba.dubbo.common.utils.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.List;
+
+/**
+ * 字典排序
+ * @author JinXing
+ * @date 2018/7/17 17:03
+ */
+public class DictionaryOrderHelper {
+
+
+    /**
+     * 字典排序
+     * 思路：
+     * 1.将列表进行自然排序 (升序)
+     * //2.将自然排序的结果进行组装并返回
+     * @author JinXing
+     * @date 2018/7/17 17:13
+     *
+     */
+    public static String getDictionaryOrder(List<String> dictionaryList){
+
+        if(CollectionUtils.isEmpty(dictionaryList))return null;
+
+        //1.将列表进行自然排序 (升序)
+        CollectionUtils.sort(dictionaryList);
+
+        //2.将自然排序的结果进行组装并返回
+        StringBuffer resultBuffer=new StringBuffer();
+        dictionaryList.stream().filter(str-> StringUtils.isNotEmpty(str)).forEach(str->
+            resultBuffer.append(str)
+        );
+        return resultBuffer.toString();
+
+    }
+
+
+
+}
