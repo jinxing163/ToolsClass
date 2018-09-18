58c5faa0dee3cfb91e8460b4264765ed494aa39e
jinxing <jinxing@able-elec.com>
2018-09-18 09:07:43 +0800

    demo

---
+++ src/main/java/com/example/tools/api/Demo.java
@@ -0,0 +1,13 @@
+package com.example.tools.api;
+
+/**
+ * @author JinXing
+ * @date 2018/9/18 9:05
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Demo................");
+    }
+
+}
